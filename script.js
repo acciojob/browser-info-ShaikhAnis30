61a434e21e0777c29207b83c53a9964e905fcf33
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-07-27 14:57:44 +0530

    Added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+console.log(navigator.userAgent)
+
+let browserInfo = navigator.userAgent;
+let arr = browserInfo.split(' ');
+// console.log(arr[0])
+let nameAndVersion = arr[0].split('/');
+let browserName = nameAndVersion[0];
+let version = nameAndVersion[1];
+console.log(browserName, version)
+
+const div = document.getElementById("browser-info");
+let message = `You are using ${browserName} version ${version} ${browserInfo}`;
+div.innerText = message;
